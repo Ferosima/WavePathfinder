7f3c9238a715e4b633662a4c934824e1b13aa97b
Serhii <shusachenko@taker.io>
2023-04-10 00:48:47 +0300

    Change Style

--- src/view/components/Cell/Cell.tsx
+++ src/view/components/Cell/Cell.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './styles.scss';
-import { Cell } from '../../../domain/Cell/Cell';
-import { observer } from 'mobx-react';
-import { useCell } from './hooks/useCell';
+import React from "react";
+import "./styles.scss";
+import { Cell } from "../../../domain/Cell/Cell";
+import { observer } from "mobx-react";
+import { useCell } from "./hooks/useCell";
 
 type TCell = {
   item: Cell;
 };
 
 export const CellComponent = observer(({ item }: TCell) => {
-  const [preset, onPress] = useCell(item);
+  const [distance, preset, onPress] = useCell(item);
 
   return (
-    <div className={`cell ${preset}`} onClick={onPress}>
-      {item.distance}
+    <div className="cell-wrapper">
+      <div className={`cell ${preset}`} onClick={onPress}>
+        {distance}
+      </div>
     </div>
   );
 });
--- src/view/components/Cell/hooks/useCell.ts
+++ src/view/components/Cell/hooks/useCell.ts
@@ -1,21 +1,23 @@
-import { Context } from '../../../App';
-import { Cell } from '../../../../domain/Cell/Cell';
-import { useContext, useCallback } from 'react';
-import { CELL_TYPES } from '../../../../types';
+import { Context } from "../../../App";
+import { Cell } from "../../../../domain/Cell/Cell";
+import { useContext, useCallback } from "react";
+import { CELL_TYPES } from "../../../../types";
 
 const preset = {
-  [CELL_TYPES.CELL]: '',
-  [CELL_TYPES.WALL]: 'wall',
-  [CELL_TYPES.DESTINATION]: 'destination',
-  [CELL_TYPES.WAY]: 'way',
+  [CELL_TYPES.CELL]: "",
+  [CELL_TYPES.WALL]: "wall",
+  [CELL_TYPES.DESTINATION]: "destination",
+  [CELL_TYPES.WAY]: "way",
 };
 
-export const useCell = (cell: Cell): [string, () => void] => {
+export const useCell = (cell: Cell): [number | null | undefined, string, () => void] => {
   const Controller = useContext(Context);
+
+  const distance = cell.distance && cell.distance > 0 ? cell.distance : null;
 
   const onPress = useCallback(() => {
     Controller?.selectCell(cell.x, cell.y);
   }, []);
 
-  return [preset[cell.type], onPress];
+  return [distance, preset[cell.type], onPress];
 };
